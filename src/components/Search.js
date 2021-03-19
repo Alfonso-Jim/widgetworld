3794e07497f074040698508c6967c8b5ada238b9
Alfonso Jim <alfonsojim1897@gmail.com>
2021-03-19 09:54:10 +0100

    fixed first search delay

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,15 +19,20 @@ const Search = () => {
 
       setResult(data.query.search);
     };
-    const timeoutId = setTimeout(() => {
-      if (term) {
-        search();
-      }
-    }, 1000);
 
-    return () => {
-      clearTimeout(timeoutId);
-    };
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
