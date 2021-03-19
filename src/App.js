ec4ddac11f60d12c2e7157b391d60e0db79db06f
Alfonso Jim <alfonsojim1897@gmail.com>
2021-03-19 05:01:14 +0100

    added Search comp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import "./App.css";
 import React from "react";
-import Accordion from "./components/Accordion";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-const items = [
-  {
-    title: "What is react?",
-    content: "React is a front end js framework",
-  },
-  {
-    title: "Why use react?",
-    content: "React is a favorite JS lib among engineers",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components",
-  },
-];
+// const items = [
+//   {
+//     title: "What is react?",
+//     content: "React is a front end js framework",
+//   },
+//   {
+//     title: "Why use react?",
+//     content: "React is a favorite JS lib among engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components",
+//   },
+// ];
 
 export default () => {
   return (
     <div className="ui segment">
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
