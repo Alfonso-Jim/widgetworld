014add75c5d0c6782bac54afa12017062d52b7fd
Alfonso Jim <alfonsojim1897@gmail.com>
2021-03-21 17:27:55 +0100

    added translated text to Convert comp

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,21 +4,28 @@ import axios from "axios";
 const key = process.env.REACT_APP_MY_GOOGLE_TRANSLATE_KEY;
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
   useEffect(() => {
-    axios.post(
-      "https://translation.googleapis.com/language/translate/v2",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: key,
-        },
-      }
-    );
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: key,
+          },
+        }
+      );
+
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
   }, [language, text]);
 
-  return <div></div>;
+  return <h1 className="ui header">{translated}</h1>;
 };
 
 export default Convert;
